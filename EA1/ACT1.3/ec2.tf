421514a39d4992b6ee35d4fa86bca9dbbbc77c2b
Ale Remoto <ale.avilac@duocuc.cl>
2026-04-16 00:39:26 +0000

    Inicio de parcial creación de rama parcial, Ajustes de: seguridad en key_pair, provider, instancia de las AMI, creación de archivos de outputs y variables, corrección de required_version según tflint

--- EA1/ACT1.3/ec2.tf
+++ EA1/ACT1.3/ec2.tf
@@ -1,8 +1,3 @@
-resource "aws_key_pair" "mi_key" {
-  key_name   = "mi_key_name"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDiuFUssdtHg8Y3rWGZFCSD58hSr4IqjFVKeid9d0G3bk7w99/AOyL/C45PnFodjOtD1eMndiCd40BqagdOYtKoieqlOTlmShrvE7N2A+MeaOP4CWLx7fj2MfekecPPFRAiMUCZk51SHxFr4oqX4Qhj8BkG1cG30p9QB+stfJKT3tUGczxUB1aor9qoLmPDTfaE4iSmNDscVmqQhX9jkppdzkg2ENh5cDO2EtLlHHxIodXLgetpWjBP68r90q/gwZV69XANcTWjZiZRyDmb9nIfQiZOO5C03FoG0GmTSZkAfvZdq7M2GsQSboln44VW/ukyQKFRVVepOCIHTaqcsjhV"
-}
-
 resource "aws_security_group" "ssh_access" {
   name        = "ssh-access"
   description = "Permitir acceso SSH desde cualquier IPv4"
@@ -30,13 +25,13 @@ resource "aws_security_group" "ssh_access" {
 }
 
 resource "aws_instance" "mi_ec2" {
-  ami                    = "ami-012967cc5a8c9f891"
+  ami                    = "ami-0ec10929233384c7f"
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.mi_key.key_name
+  key_name               = "vockey"
   subnet_id              = aws_subnet.subnet_publica_1.id
   vpc_security_group_ids = [aws_security_group.ssh_access.id]
 
   tags = {
-    Name = "MiInstancia"
+    Name = "MiInstancia Terraformeada"
   }
 }
--- EA1/ACT1.3/provider.tf
+++ EA1/ACT1.3/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0" # Esto acepta cualquier versión mayor o igual a la 1.0.0
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Es recomendable también fijar la versión del proveedor
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
